9787ebc85947e0582b167f91f200621eba86992c
Jath-Alison <Jath.Alison@gmail.com>
2024-06-26 21:21:19 -0500

    I was a fool (forgot to change indentation style)

--- include/vex.h
+++ include/vex.h
@@ -21,14 +21,14 @@
 #include "v5.h"
 #include "v5_vcs.h"
 
-extern vex::brain Brain;            /**< a brain object representing the V5 Brain */
+extern vex::brain Brain;			/**< a brain object representing the V5 Brain */
 extern vex::controller Controller1; /**< a controller object representing the Primary Controller */
 
 #define waitUntil(condition) \
-  do                         \
-  {                          \
-    wait(5, msec);           \
-  } while (!(condition))
+	do                       \
+	{                        \
+		wait(5, msec);       \
+	} while (!(condition))
 
 #define repeat(iterations) \
-  for (int iterator = 0; iterator < iterations; iterator++)
+	for (int iterator = 0; iterator < iterations; iterator++)
