b963c50319a0795b13ac684ef1f3f790e7407ab0
Jath-Alison <Jath.Alison@gmail.com>
2024-06-27 14:10:56 -0500

    SimpleMotor written and documented, needs validation & guide

---
+++ include/ART/ART.h
@@ -0,0 +1,17 @@
+/**
+ * @file ART.h
+ * @author Jath Alison (Jath.Alison@gmail.com)
+ * @brief A file containing includes to the entire ART library
+ * @version 0.1
+ * @date 06-27-2024
+ *
+ * @copyright Copyright (c) 2024
+ *
+ * Including this header will give access to the entire ART library in one file.
+ * If desired, you can include only the parts you want to use, but this header
+ * gives eveything in one include, making it easy to include the whole library.
+ */
+
+#pragma once
+
+#include "ART/SimpleMotor.h"
