df369e9c1ec1caccbe99f5d420d67188c8ff0276
Jath-Alison <Jath.Alison@gmail.com>
2024-07-25 12:15:52 -0500

    still trying to put docusaurus in subdirectory

--- Site/ART_Tutorials/docusaurus.config.js
+++ Site/ART_Tutorials/docusaurus.config.js
@@ -8,20 +8,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Tutorials',
+  tagline: 'A Work of ART',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Jath-Alison.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ART/ART_Tutorials',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Jath-Alison', // Usually your GitHub org/user name.
+  projectName: 'ART', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
