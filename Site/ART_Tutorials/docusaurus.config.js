7d42616f4fc996614adff490433b5b57499810d1
Jath-Alison <Jath.Alison@gmail.com>
2024-07-25 17:47:56 -0500

    fixed and deployed docusaurus, need to add doc page

--- Site/ART_Tutorials/docusaurus.config.js
+++ Site/ART_Tutorials/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://Jath-Alison.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ART/ART_Tutorials',
+  baseUrl: '/ART/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
